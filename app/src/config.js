79541affe50db23983307d745ff343efdfc742f1
prudowicz <prudowicz@protonmail.com>
2019-12-04 15:19:10 +0100

    Adding files to branch lambdas

---
+++ app/src/config.js
@@ -0,0 +1,30 @@
+export default {
+    s3: {
+        REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+        REGION: "YOUR_API_GATEWAY_REGION",
+        URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: ,
+        USER_POOL_ID: ,
+        APP_CLIENT_ID: ,
+        IDENTITY_POOL_ID: 
+    }
+};
+
+// export default {
+//     s3: {
+//         REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+//         BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+//     },
+//     apiGateway: {
+//         REGION: "YOUR_API_GATEWAY_REGION",
+//         URL: "YOUR_API_GATEWAY_URL"
+//     },
+//     cognito: {
+//         
+//     }
+// };
