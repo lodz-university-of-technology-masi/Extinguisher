bce80284aa67f0a21e1a4ad355cefa380988ef61
AdamGniadzik <56366799+AdamGniadzik@users.noreply.github.com>
2020-01-08 20:12:51 +0100

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -7,25 +7,4 @@ export default {
         REGION: "YOUR_API_GATEWAY_REGION",
         URL: "YOUR_API_GATEWAY_URL"
     },
-    cognito: {
-        REGION: "us-east-1",
-        USER_POOL_ID: "us-east-1_M3dMBNpHE",
-        APP_CLIENT_ID: "34t0gn04kbd889cj9pubtrpn88",
-        IDENTITY_POOL_ID: "us-east-1:1e73f9d4-4448-4fee-93e5-5672e89314a6"
-    }
 };
-
-// export default {
-//     s3: {
-//         REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-//         BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
-//     },
-//     apiGateway: {
-//         REGION: "YOUR_API_GATEWAY_REGION",
-//         URL: "YOUR_API_GATEWAY_URL"
-//     },
-//     cognito: {
-
-//         
-//     }
-// };
