b9c2dcd142f5a241cdbb12405314d1918746ef21
AdamGniadzik <56366799+AdamGniadzik@users.noreply.github.com>
2020-01-08 20:26:34 +0100

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -2,9 +2,5 @@ export default {
     s3: {
         REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
         BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
-    },
-    apiGateway: {
-        REGION: "us-east-1",
-        URL: "us-east-1:374991088908:wjdhyrfow4"
-    },
+    }
 };
