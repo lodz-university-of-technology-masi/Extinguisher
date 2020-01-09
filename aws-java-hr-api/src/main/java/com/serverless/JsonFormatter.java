3b5bac1dc627edca494c85ce693cfa598553e0b3
AdamGniadzik <56366799+AdamGniadzik@users.noreply.github.com>
2020-01-10 00:28:04 +0100

    cuda

--- aws-java-hr-api/src/main/java/com/serverless/JsonFormatter.java
+++ aws-java-hr-api/src/main/java/com/serverless/JsonFormatter.java
@@ -11,80 +11,7 @@ import java.util.UUID;
 
 public class JsonFormatter {
 
-    protected static String getCandidatesAsJsonString(
-            String username, String answers, boolean passed, boolean finished, boolean rated, int points, Item test) throws IOException {
-        String result = "[";
-        Iterator<JsonNode> candidates = new ObjectMapper().readValue(test.getJSONPretty("candidates"), JsonNode.class).iterator();
-        while (candidates.hasNext()) {
-            JsonNode candidate = candidates.next();
-            if (!candidate.get("username").asText().contains(username)) {
-                result += getCandidateAsJsonString(candidate);
-            } else {
-                result += getCandidateAsJsonString(username, answers, passed, finished, rated, points);
-            }
-            result += candidates.hasNext() == true ? "," : "";
-        }
-        result += "]";
-        return result;
-    }
 
-    protected static String getCandidateAsJsonString(JsonNode candidate) {
-        String answersAsText = getCandidateAnswersAsJsonString(candidate.get("answers"));
-        String result = "{" +
-                "\"username\":\"" + candidate.get("username").asText() + "\"," +
-                "\"answers\":" + answersAsText + "," +
-                "\"passed\":" + candidate.get("passed").asBoolean() + "," +
-                "\"finished\":" + candidate.get("finished").asBoolean() + "," +
-                "\"rated\":" + candidate.get("rated").asBoolean() + "," +
-                "\"points\":" + candidate.get("points").asInt() +
-                "}";
-        return result;
-    }
-
-    protected static String getCandidateAsJsonString(String username, String answers, boolean passed, boolean finished, boolean rated, int points) {
-        String result = "{" +
-                "\"username\":\"" + username + "\"," +
-                "\"answers\":" + answers + "," +
-                "\"passed\":" + passed + "," +
-                "\"finished\":" + finished + "," +
-                "\"rated\":" + rated + "," +
-                "\"points\":" + points +
-                "}";
-        return result;
-    }
-
-    protected static String getCandidateAnswersAsJsonString(JsonNode answers) {
-        String json = "[";
-        for (int i = 0; i < answers.size(); i++) {
-            JsonNode answer = answers.get(i);
-            json += getCandidateAnswerAsJsonString(answer);
-            json += i != answers.size() - 1 ? "," : "";
-        }
-        json += "]";
-        return json;
-    }
-
-    protected static String getCandidateAnswerAsJsonString(JsonNode answer) {
-        String result = "{" +
-                "\"question\": \"" + answer.get("question").asText() + "\"," +
-                "\"type\": \"" + answer.get("type").asText() + "\"," +
-                "\"content\": \"" + answer.get("content").asText() + "\"," +
-                "\"correct\": \"" + answer.get("correct").asText() + "\"," +
-                "\"rated\": \"" + answer.get("rated").asText() + "\"" +
-                "}";
-        return result;
-    }
-
-    protected static String getCandidateAnswerAsJsonString(String question, String type, String content, boolean correct, boolean rated) {
-        String result = "{" +
-                "\"question\": \"" + question + "\"," +
-                "\"type\": \"" + type + "\"," +
-                "\"content\": \"" + content + "\"," +
-                "\"correct\": \"" + correct + "\"," +
-                "\"rated\": \"" + rated + "\"" +
-                "}";
-        return result;
-    }
 
     protected static String getQuestionsAsJsonString(JsonNode rootNode) {
         String questions = new String("[");
@@ -122,32 +49,4 @@ public class JsonFormatter {
         return questions;
     }
 
-    protected static String removeAttributeFromTest(String item, String attribute) throws IOException {
-        JsonNode test = new ObjectMapper().readValue(item, JsonNode.class);
-        ((ObjectNode) test).remove(attribute);
-        return test.toString();
-    }
-
-    protected static String removeCandidatesFromTest(String itemAsString) throws IOException {
-        JsonNode test = new ObjectMapper().readValue(itemAsString, JsonNode.class);
-        ((ObjectNode) test).remove("candidates");
-        return test.toString();
-    }
-
-    protected static String removeCorrectAnswersFromTest(String itemAsString) throws IOException {
-        JsonNode test = new ObjectMapper().readValue(itemAsString, JsonNode.class);
-        JsonNode questions = test.get("questions");
-        for (int i = 0; i < questions.size(); i++) {
-            JsonNode question = questions.get(i);
-            if (question.get("question_type").asText().contains("W")) {
-                JsonNode answers = question.get("answers");
-                for (int j = 0; j < answers.size(); j++) {
-                    ((ObjectNode) answers.get(j)).remove("correct");
-                }
-            } else if (question.get("question_type").asText().contains("L")) {
-                ((ObjectNode) question).remove("correct_answer");
-            }
-        }
-        return test.toString();
-    }
 }
